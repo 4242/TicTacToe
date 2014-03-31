fbd0fffa54440a074bedac1eb5266a7bf4b70afc
elv1s42 <42_@mail.ru>
2014-04-01 00:36:44 +0400

    в рот отъебись

--- TicTacToe/src/main/java/com/organization4242/tictactoe/ai/TicTacToeAI.java
+++ TicTacToe/src/main/java/com/organization4242/tictactoe/ai/TicTacToeAI.java
@@ -1,12 +1,35 @@
 package com.organization4242.tictactoe.ai;
 
+import com.organization4242.tictactoe.model.CheckForWin;
+import com.organization4242.tictactoe.model.MainField;
+
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by ilya on 31.03.14.
  */
 public class TicTacToeAI {
-    public static Byte nextMove(List<Integer> state, Integer field, Boolean order) {
-        return 1;
+    public static Byte[] nextMove(MainField state) {
+        int activeField = state.getActiveField();
+        boolean order = state.getOrder();
+        Byte[] move = new Byte[]{0,0};
+        List<List<Integer>> templateField = new ArrayList<List<Integer>>();
+        List<List<Integer>> inspectedField = new ArrayList<List<Integer>>();
+        templateField.addAll(state.getFields().get(activeField));
+        for (int i = 0; i < 3; i++) {
+            for(int j = 0; j < 3; j++) {
+                inspectedField.addAll(templateField);
+                if(state.getFields().get(activeField).get(i).get(j) == 0) {
+                    inspectedField.get(i).set(j, 1);
+                    if(CheckForWin.IsWin(inspectedField) == 1) {
+                        move = new Byte[]{(byte) i,(byte) j};
+                        return move;
+                    }
+                }
+            }
+        }
+        return move;
+
     }
 }
--- TicTacToe/src/main/java/com/organization4242/tictactoe/model/CheckForWin.java
+++ TicTacToe/src/main/java/com/organization4242/tictactoe/model/CheckForWin.java
@@ -6,7 +6,7 @@ import java.util.List;
  * Created by Евгений on 31.03.2014.
  */
 public class CheckForWin {
-    public static boolean IsWin (List<List<Integer>> matrix, boolean order) {
+    public static boolean IsWinBoolean (List<List<Integer>> matrix, boolean order) {
         int sums[] = {0,0,0,0,0,0,0,0};
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
@@ -21,5 +21,26 @@ public class CheckForWin {
             if(!order && sums[i]==-3) return true;
         }
         return false;
+    }
+    public static Byte IsWin (List<List<Integer>> matrix) {
+        int sums[] = {0,0,0,0,0,0,0,0};
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                sums[i]+=matrix.get(i).get(j);
+                sums[i+3]+=matrix.get(j).get(i);
+            }
+            sums[6]+=matrix.get(i).get(i);
+            sums[7]+=matrix.get(i).get(3-i);
+        }
+        for (int i = 0; i < 8; i++) {
+            if(sums[i] ==-3) {
+                return -1;
+            } else if (sums[i]== 3) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+        return 0;
     }
 }
