f5a4e3dcc7bbe1c17468ae6de7e654785683c81e
ilya-murzinov <murz42@gmail.com>
2014-04-05 22:53:38 +0400

    added dummy getWinner implementation to Field

--- TicTacToe/src/main/java/com/organization4242/tictactoe/model/Field.java
+++ TicTacToe/src/main/java/com/organization4242/tictactoe/model/Field.java
@@ -8,18 +8,38 @@ import java.util.ArrayList;
 public class Field implements FieldInterface {
     private ArrayList<State> list;
 
+    private boolean checkWinner(State order) {
+        ArrayList<Byte> list = new ArrayList<Byte>(MainFieldModel.NUMBER_OF_FIELDS);
+        for (State state : this.list) {
+            list.add(State.toByte(state));
+        }
+
+        return list.get(0) + list.get(1) + list.get(2) == 3 * State.toByte(order)
+                || list.get(3) + list.get(4) + list.get(5) == 3 * State.toByte(order)
+                || list.get(6) + list.get(7) + list.get(8) == 3 * State.toByte(order)
+                || list.get(0) + list.get(4) + list.get(8) == 3 * State.toByte(order)
+                || list.get(2) + list.get(4) + list.get(6) == 3 * State.toByte(order)
+                || list.get(6) + list.get(7) + list.get(8) == 3 * State.toByte(order);
+
+    }
+
     public Field(int capacity) {
         list = new ArrayList<State>(capacity);
     }
 
     @Override
     public State getWinner() {
-        return State.X;
+        if (checkWinner(State.X)) {
+            return State.X;
+        } else if (checkWinner(State.O)) {
+            return State.O;
+        }
+        return State.EMPTY;
     }
 
     @Override
     public boolean hasWinner() {
-        return false;
+        return checkWinner(State.O) || checkWinner(State.X);
     }
 
     @Override
